21ac535ca00d822b07659827bb8f65945cc2ab85
Dale-Kurt Murray <dalekurtm@adobe.com>
2024-06-25 15:14:38 -0400

    Fix

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
-import sqlite3
-import tenacity
 import logging
+import sqlite3
+
+import tenacity
+
 from model import Product
+
 DATABASE_FILE = "db.sqlite"
+
 
 @tenacity.retry(wait=tenacity.wait_fixed(5))
 def connect_database():
     con = sqlite3.connect(DATABASE_FILE, check_same_thread=False)
     return con
+
 
 def get_products(db_connection, limit, offset):
     products = []
@@ -16,7 +21,7 @@ def get_products(db_connection, limit, offset):
     for v in res:
         products.append(Product(v[0], v[1]))
 
-    return products[offset:offset+limit]
+    return products[offset : offset + limit]
 
 
 def add_product(db_connection, product: Product):
@@ -24,14 +29,16 @@ def add_product(db_connection, product: Product):
         cursor = db_connection.cursor()
         # This is a potential SQL injection, as the product name could be directly passed from the user.
         # replace it with the line below:
-        # cursor.execute("INSERT INTO products (id, title) VALUES (NULL, ?)", (product.name, ))
-        cursor.execute("INSERT INTO products (id, title) VALUES (NULL, '" + product.name+"');")
+        cursor.execute(
+            "INSERT INTO products (id, title) VALUES (NULL, ?)", (product.name,)
+        )
+        # cursor.execute("INSERT INTO products (id, title) VALUES (NULL, '" + product.name+"');")
         db_connection.commit()
         return True
     # A bare exception fails to captures the exact exceptions (and print it). The code should rather
     # catch specific exceptions.
     # https://docs.datadoghq.com/code_analysis/static_analysis_rules/python-best-practices/no-bare-except/
-    # Replace with the following: 
+    # Replace with the following:
     # except sqlite3.IntegrityError:
     except:
         return False
